a0539cb5a898bbd02245e66a5db501e6a01c4972
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 16:33:14 +0000

    Functions refactor

--- src/script.js
+++ src/script.js
@@ -6,21 +6,20 @@ function addApi(response) {
   });
 }
 
+function getInput(event) {
+  let input = event.target.value;
+
+  let prompt = `Generate a poem about ${input} in HTML in this format: <h2 class="poem-title">title</h2><p class="poem">poem</p>`;
+  let context = `Accordingly to your knowledge, generate a poem with two stanzas `;
+  let apiKey = "8bc029ce07bb99a925obf42d966t543f";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(addApi);
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  function getInput(event) {
-    let input = event.target.value;
-    if (event.target.value) {
-      let prompt = `Generate a poem about ${input} in HTML in this format: <h2>title</h2><p>poem</p>`;
-      let context = `Accordingly to your knowledge, generate a poem with two stanzas `;
-      let apiKey = "8bc029ce07bb99a925obf42d966t543f";
-      let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-      axios.get(apiUrl).then(addApi);
-    }
-  }
-  console.log("getting poem");
   let inputSearch = document.querySelector("#search-txt");
   inputSearch.addEventListener("change", getInput);
 }

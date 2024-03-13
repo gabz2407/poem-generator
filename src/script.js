5df2e129cf5a557290c0393c6dab024a8dc0f03e
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 16:15:01 +0000

    Added Typewriter.js  and input.value

--- src/script.js
+++ src/script.js
@@ -1,17 +1,28 @@
 function addApi(response) {
-  let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = response.data.answer;
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+  });
 }
 
 function generatePoem(event) {
   event.preventDefault();
 
-  let prompt = `Generate a poem about bread`;
-  let context = `Accordingly to your knowledge, generate a poem with two stanzas`;
-  let apiKey = "8bc029ce07bb99a925obf42d966t543f";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  function getInput(event) {
+    let input = event.target.value;
+    if (event.target.value) {
+      let prompt = `Generate a poem about ${input} in HTML in this format: <h2>title</h2><p>poem</p>`;
+      let context = `Accordingly to your knowledge, generate a poem with two stanzas `;
+      let apiKey = "8bc029ce07bb99a925obf42d966t543f";
+      let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(addApi);
+      axios.get(apiUrl).then(addApi);
+    }
+  }
+  console.log("getting poem");
+  let inputSearch = document.querySelector("#search-txt");
+  inputSearch.addEventListener("change", getInput);
 }
 
 let poemForm = document.querySelector("#generator-form");

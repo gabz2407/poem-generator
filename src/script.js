8ebd64ad5ca6584bf7db8ef457561f9baae731dc
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 17:19:47 +0000

    css poems styling

--- src/script.js
+++ src/script.js
@@ -3,14 +3,15 @@ function addApi(response) {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
+    cursor: "",
   });
 }
 
 function getInput(event) {
   let input = event.target.value;
 
-  let prompt = `Generate a poem about ${input} in HTML in this format: <h2 class="poem-title">title</h2><p class="poem">poem</p>`;
-  let context = `Accordingly to your knowledge, generate a poem with two stanzas `;
+  let prompt = `Generate a poem about ${input} in this format: <h2 class="poem-title">title</h2><p class="poem-text">poem</p>`;
+  let context = `Accordingly to your knowledge, generate a beautiful poem with two stanzas `;
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
@@ -21,7 +22,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   let inputSearch = document.querySelector("#search-txt");
-  inputSearch.addEventListener("change", getInput);
+  inputSearch.addEventListener("search", getInput);
 }
 
 let poemForm = document.querySelector("#generator-form");

381f27e0309f5fe384b11e100f558073df8d4ead
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 15:41:52 +0000

    Added axios and API

---
+++ src/script.js
@@ -0,0 +1,17 @@
+function addApi(response) {
+  console.log(response.data);
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let prompt = `Generate a poem about bread`;
+  let context = `Accordingly to your knowledge, generate a poem with two stanzas`;
+  let apiKey = "8bc029ce07bb99a925obf42d966t543f";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(addApi);
+}
+
+let poemForm = document.querySelector("#generator-form");
+poemForm.addEventListener("submit", generatePoem);

90f8beadd07515369a39d0064a195e69088f03f7
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 18:52:28 +0000

    Css improvement

--- src/script.js
+++ src/script.js
@@ -9,13 +9,15 @@ function addApi(response) {
 
 function getInput(event) {
   event.preventDefault();
-  let input = event.target.value;
+  let input = document.querySelector("#search-txt");
+  input = input.value;
 
   let poem = document.querySelector("#poem-container");
   poem.classList.remove("hidden");
+  console.log(input);
 
-  let prompt = `Generate a poem about ${input} in this format: <h2 class="poem-title">title</h2><p class="poem-text">poem</p>`;
-  let context = `Accordingly to your knowledge, generate a beautiful poem with two stanzas `;
+  let prompt = `Generate a poem about ${input} and ALWAYS sign as "<strong>AI Poem Generator</b>" please`;
+  let context = `Accordingly to your knowledge, generate a beautiful poem with two stanzas and a single title in this format: <h2 class="poem-title">title</h2><p class="poem-text">poem verse </br> poem verse</br> poem verse</br> poem verse</p>`;
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 

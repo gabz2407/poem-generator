07b78955e18afbd6b4db9dad098648a8cb945bf7
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-13 17:47:45 +0000

    Improved UX

--- src/script.js
+++ src/script.js
@@ -10,6 +10,10 @@ function addApi(response) {
 function getInput(event) {
   let input = event.target.value;
 
+  let poem = document.querySelector("#poem-container");
+  poem.classList.remove("hidden");
+  poem.innerHTML = "Generating poem...";
+
   let prompt = `Generate a poem about ${input} in this format: <h2 class="poem-title">title</h2><p class="poem-text">poem</p>`;
   let context = `Accordingly to your knowledge, generate a beautiful poem with two stanzas `;
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";

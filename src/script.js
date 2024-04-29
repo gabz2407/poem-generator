0bbe1237565b6786297701fa7cbf7e8790ebf1c7
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-29 23:36:44 +0100

    added two stanzas to poem

--- src/script.js
+++ src/script.js
@@ -17,7 +17,7 @@ function getInput(event) {
   console.log(input);
 
   let prompt = `Generate a poem about ${input} and ALWAYS sign as "<strong>AI Poem Generator</b>" please`;
-  let context = `Accordingly to your knowledge, generate a beautiful poem with two stanzas and a single title in this format: <h2 class="poem-title">title</h2><p class="poem-text">poem verse </br> poem verse</br> poem verse</br> poem verse</p>`;
+  let context = `Accordingly to your knowledge, generate a beautiful poem with two stanzas and a single title in this format: <h2 class="poem-title">title</h2><p class="poem-text">poem verse </br> poem verse</br> poem verse</br> poem verse</p><p class="poem-text">poem verse </br> poem verse</br> poem verse</br> poem verse</p>`;
   let apiKey = "8bc029ce07bb99a925obf42d966t543f";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
